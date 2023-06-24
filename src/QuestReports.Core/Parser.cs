f90432ef6e7b3e8e4693a51c3192d2eb98ec4b59
daniilda <daniilda@hotmail.com>
2023-06-24 12:21:51 +0300

    wip 1

---
+++ src/QuestReports.Core/Parser.cs
@@ -0,0 +1,38 @@
+﻿using System.Xml;
+using QuestPDF.Infrastructure;
+using QuestPDF.Fluent;
+using QuestPDF.Helpers;
+using QuestPDF.Infrastructure;
+
+namespace QuestReports.Core;
+
+public class Parser
+{
+    public void Parse(string str)
+    {
+        var doc = new XmlDocument();
+        var stringReader = new StringReader(str);
+        var reader = XmlReader.Create(stringReader);
+        doc.Load(reader);
+        var root = doc.DocumentElement;
+        Document.Create(document =>
+        {
+            foreach (XmlElement childNode in root!.ChildNodes)
+            {
+                switch (childNode.Name)
+                {
+                    case "page":
+                    {
+                        document.Page(page =>
+                        {
+                            foreach (XmlElement child in childNode)
+                            {
+                            }
+                        });
+                        break;
+                    }
+                }
+            }
+        });
+    }
+}
---
+++ tests/QuestReports.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace QuestReports.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
